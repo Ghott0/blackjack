85ede6a27c69239ba19ee5049aab7fce2a5fdddf
Ghott0 <ghottt0@gmail.com>
2022-05-23 18:32:15 -0300

    css

--- index.js
+++ index.js
@@ -5,23 +5,22 @@ let secondCard = 10
 let hand = firstCard + secondCard
 let message = ""
 
-
-if (hand === 21)  {
-    message = "Get in there! Blackjack!"
-    hasBlackJack = true
+function startGame() {
+    if (hand === 21)  {
+        message = "Get in there! Blackjack!"
+        hasBlackJack = true
+    }
+    
+    else if (hand > 21)  {
+        message = "No Mikey, no! Bust!"
+        isAlive = false
+    }
+    
+    else {
+        message = "Would you like to draw another card?"
+    }
+    
+    console.log(message)
 }
 
-else if (hand > 21)  {
-    message = "No Mikey, no! Bust!"
-    isAlive = false
-}
 
-else {
-    message = "Would you like to draw another card?"
-}
-
-console.log(message)
-
-console.log(hasBlackJack)
-
-console.log(isAlive)
